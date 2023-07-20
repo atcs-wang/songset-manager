0e9cbb003d743b7210d33cc2ba2a51f8d346faee
M Wang <wang.matthew.h@gmail.com>
2023-07-20 13:02:54 -0400

    songs table and api design

--- db/db_create_song.sql
+++ db/db_create_song.sql
@@ -1,52 +1,37 @@
-CREATE TABLE `key` (
-  `key` varchar(15) NOT NULL,
-  PRIMARY KEY (`key`)
-);
+-- For now, keys are just text. 
+-- Possibly add fixecd set of keys with options
+
+-- CREATE TABLE `key` (
+--   `key` varchar(15) NOT NULL,
+--   PRIMARY KEY (`key`)
+-- );
+
+-- insert into `key` (key) 
+-- values ("C major"), ("Db major"), ("D major"), ("Eb major"), ("E major"), ("F major"), ("F# major"), ("G major"), ("Ab major"), ("A major"), ("Bb major"), ("B major"),
+-- ("C minor"), ("C# minor"), ("D minor"), ("D# minor"), ("E minor"), ("F minor"), ("F# minor"), ("G minor"), ("G# minor"), ("A minor"), ("Bb minor"), ("B major");
+
 
 CREATE TABLE `song` (
   `song_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `artist` varchar(255) DEFAULT NULL,
-  `std_key` varchar(15)  DEFAULT NULL,
-  -- `key_alt` varchar(15)  DEFAULT NULL,
+  `title` varchar(45) NOT NULL,
+  `artist` varchar(100) DEFAULT NULL,
+  `key` varchar(30)  DEFAULT NULL,
+  `tags` varchar(100) DEFAULT NULL,
   `notes` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_id` varchar(255) NOT NULL,
+  `creator_id` varchar(255) NULL,
+  `band_id` int(11) NOT NULL,
   PRIMARY KEY (`song_id`),
-  KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `fk_song_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  KEY `std_key_idx` (`std_key`),
-  CONSTRAINT `fk_song_std_key` FOREIGN KEY (`std_key`) REFERENCES `key` (`key`) ON DELETE SET NULL ON UPDATE CASCADE
-  -- KEY `key_alt_idx` (`key_alt`),
-  -- CONSTRAINT `fk_song_key_alt` FOREIGN KEY (`key`) REFERENCES `key` (`key`) ON DELETE SET NULL ON UPDATE CASCADE
+  KEY `creator_id_idx` (`creator_id`),
+  CONSTRAINT `fk_song_creator_id` FOREIGN KEY (`creator_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  -- KEY `key_idx` (`key`),
+  -- CONSTRAINT `fk_song_key` FOREIGN KEY (`key`) REFERENCES `key` (`key`) ON DELETE SET NULL ON UPDATE CASCADE
+  KEY `band_id_idx` (`band_id`),
+  CONSTRAINT `fk_ubr_band_id` FOREIGN KEY (`band_id`) REFERENCES `band` (`band_id`) ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
--- Possibly add multiple keys with notes later
-
-CREATE TABLE `category` (
-  `category_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  -- `song_count` int(11) NOT NULL DEFAULT '0', -- add with triggers later
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_id` varchar(255) NOT NULL ,
-  -- 'band_id' int(11) NOT NULL, -- change to band ownership later
-  PRIMARY KEY (`category_id`),
-  KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `fk_category_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE `song_category_xref` (
-  `song_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`song_id`,`category_id`),
-  KEY `category_id_idx` (`category_id`),
-  CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  KEY `song_id_idx` (`song_id`),
-  CONSTRAINT `song_id` FOREIGN KEY (`song_id`) REFERENCES `song` (`song_id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
+-- Design decision -  Just keep tags as denormalized comma separated values in a single field under song, since tags are short.
 
 
 
--- db/songs_api.js
+++ db/songs_api.js
@@ -1,59 +1,75 @@
-// Function wrappers for database queries.
+// Function wrappers for database queries related to songs.
 // Each returns a PROMISE of database results
+// As a rule, the error handling and authentication aspects of each query are NOT handled here.
+// However, as a safety check, the band_id is also required for all queries.
 
 const db = require('./dp_pool');
 
-// Get songs info, including concatenated list of categories
-const getAllSongsSQL = `
-select song_id, title, artist, key, group_concat(c.name) from song s
-join song_category_xref x on s.song_id = x.song_id
-join category c on x.category_id = c.category_id 
-where s.user_id = ?
-group by song_id 
+// Get a song's info
+const getSongSQL = `
+select song_id, title, artist, key, tags, notes, created_at, updated_at,  
+from song s
+where song_id = ? and band_id = ?
+limit 1
+`
+function getSong(song_id, band_id) {
+    return db.execute(getSongSQL, [song_id, band_id]);
+}
+
+// Get all songs associated with a band, including concatenated list of tags
+// Only include the beginning of the notes
+const getSongsByBandSQL = `
+select song_id, title, artist, key, tags, substring(notes, 0, 20), created_at, updated_at, group_concat(t.tag SEPARATOR ", ") from song s
+from song s
+where band_id = ?
 `
 
-function getAllSongs(user_id) {
-    return db.execute(getAllSongsSQL, [user_id]);
+function getSongsByBand(band_id) {
+    return db.execute(getSongsByBandSQL, [band_id]);
 }
 
-const createSongSQL = `INSERT INTO song
-(name,artist,key,notes,user_id)
-VALUES (?,?,?,?,?);
+// Create a new song, associated with a given band by the given user.
+const createSongSQL = `insert into song
+(title,artist,key,tags,notes,creator_id,band_id)
+values (?,?,?,?,?,?,?);
 `
 
-function createSong(name, artist, key, notes, user_id) {
-    return db.execute(createSongSQL, [name, artist, key, notes, user_id])
+function createSong(title,artist,key,tags,notes,user_id, band_id) {
+    return db.execute(createSongSQL, [title,artist,key,tags,notes,user_id, band_id])
 }
 
-const deleteSongCategoriesSQL_start = `DELETE FROM song_category_xref WHERE song_id NOT IN `;
+const updateSongSQL = `update song 
+set title = ?,
+artist = ?,
+key = ?,
+tags = ?,
+notes = ?,
+where song_id = ? and band_id = ?`
 
-const addSongCategoriesSQL_start = `INSERT IGNORE INTO song_category_xref (song_id, category_id) VALUES `
-
-async function updateSongCategories(song_id, category_id_list, user_id){
-    let connection = await db.getConnection();
-    //build list 
-    let deleteResult = await connection.execute(deleteSongCategoriesSQL_start) // TODO
-    let addResult = await connection.execute(addSongCategoriesSQL_start)    
+function updateSong(song_id, band_id, title,artist,key,tags,notes){
+    return db.execute(updateSongSQL, [title,artist,key,tags,notes, song_id, band_id]);
 }
 
-const updateSongSQL = "UPDATE song WHERE user_id = ? AND song_id = ?"
+const deleteSongSQL = "delete from song where song_id = ? and band_id = ?"
 
-
-function updateSong(user_id, song_id){
-    return db.execute(updateSongSQL, [user_id]);
+function deleteSong(song_id, band_id){
+    return db.execute(deleteSongSQL, [song_id, band_id])
 }
 
-const deleteSongSQL = "DELETE FROM song WHERE user_id = ? AND song_id = ?"
+const copySongToBandSQL = `insert into song
+(title,artist,key,tags,notes,creator_id,band_id)
+select title,artist,key,tags,notes,creator_id, ? from song 
+where song_id = ? and band_id = ?`;
 
-function deleteSong(user_id, song_id){
-    return db.execute(deleteSongSQL, [user_id, song_id])
+function copySongToBand(song_id, band_id, new_band_id) {
+    return db.execute(copySongToBandSQL, [new_band_id, song_id, band_id])
 }
-
-
-
-
-function 
 
 module.exports = {
-
+    getSong,
+    getSongsByBand,
+    createSong,
+    updateSong,
+    deleteSong,
+    copySongToBand
 }
