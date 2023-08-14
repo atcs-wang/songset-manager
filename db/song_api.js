a009e2472b0185d6678a244d21e48eef1107d603
M Wang <wang.matthew.h@gmail.com>
2023-08-14 14:25:19 -0400

    song api

--- db/song_api.js
+++ db/song_api.js
@@ -7,11 +7,12 @@ const db = require('./dp_pool');
 
 // Get a song's info
 const getSongSQL = `
-select song_id, title, artist, key, tags, notes, created_at, updated_at,  
+select song_id, title, artist, key, tempo, tags, notes, created_at, updated_at 
 from song s
-where song_id = ? and band_id = ?
+where s.song_id = ? and s.band_id = ?
 limit 1
-`
+`;
+
 function getSong(song_id, band_id) {
     return db.execute(getSongSQL, [song_id, band_id]);
 }
@@ -19,10 +20,10 @@ function getSong(song_id, band_id) {
 // Get all songs associated with a band, including concatenated list of tags
 // Only include the beginning of the notes
 const getSongsByBandSQL = `
-select song_id, title, artist, key, tags, substring(notes, 0, 20), created_at, updated_at, group_concat(t.tag SEPARATOR ", ") from song s
+select song_id, title, artist, key, tempo, tags, notes, created_at, updated_at
 from song s
 where band_id = ?
-`
+`;
 
 function getSongsByBand(band_id) {
     return db.execute(getSongsByBandSQL, [band_id]);
@@ -30,39 +31,40 @@ function getSongsByBand(band_id) {
 
 // Create a new song, associated with a given band by the given user.
 const createSongSQL = `insert into song
-(title,artist,key,tags,notes,creator_id,band_id)
-values (?,?,?,?,?,?,?);
-`
+(title,artist,key,tempo,tags,notes,creator_id,band_id)
+values (?,?,?,?,?,?,?,?)
+`;
 
-function createSong(title,artist,key,tags,notes,user_id, band_id) {
-    return db.execute(createSongSQL, [title,artist,key,tags,notes,user_id, band_id])
+function createSong(user_id, band_id, title,artist,key,tempo,tags,notes) {
+    return db.execute(createSongSQL, [(title,artist,key,tempo,tags,notes,user_id,band_id])
 }
 
 const updateSongSQL = `update song 
 set title = ?,
 artist = ?,
 key = ?,
+tempo = ?,
 tags = ?,
 notes = ?,
-where song_id = ? and band_id = ?`
+where song_id = ? and band_id = ?`;
 
-function updateSong(song_id, band_id, title,artist,key,tags,notes){
-    return db.execute(updateSongSQL, [title,artist,key,tags,notes, song_id, band_id]);
+function updateSong(song_id, band_id, title,artist,key,tempo,tags,notes,){
+    return db.execute(updateSongSQL, [title,artist,key,tempo,tags,notes, song_id, band_id]);
 }
 
-const deleteSongSQL = "delete from song where song_id = ? and band_id = ?"
+const deleteSongSQL = "delete from song where song_id = ? and band_id = ?";
 
 function deleteSong(song_id, band_id){
     return db.execute(deleteSongSQL, [song_id, band_id])
 }
 
 const copySongToBandSQL = `insert into song
-(title,artist,key,tags,notes,creator_id,band_id)
-select title,artist,key,tags,notes,creator_id, ? from song 
+(title,artist,key,tempo,tags,notes,creator_id, band_id)
+select title,artist,key,tempo,tags,notes, ?, ? from song 
 where song_id = ? and band_id = ?`;
 
-function copySongToBand(song_id, band_id, new_band_id) {
-    return db.execute(copySongToBandSQL, [new_band_id, song_id, band_id])
+function copySongToBand(user_id, song_id, from_band_id, to_band_id) {
+    return db.execute(copySongToBandSQL, [user_id, to_band_id, song_id, from_band_id])
 }
 
 module.exports = {
