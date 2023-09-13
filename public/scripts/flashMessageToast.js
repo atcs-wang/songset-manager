d10d1cdf57c9a9c380c905bb5bcc51d3fe8ae055
M Wang <wang.matthew.h@gmail.com>
2023-09-12 22:00:37 -0400

    add flash error messages

--- public/scripts/flashMessageToast.js
+++ public/scripts/flashMessageToast.js
@@ -1,5 +1,14 @@
 document.querySelectorAll(".flash-info").forEach(
     (elm) => {
-        M.toast({html: elm.innerHTML})
+        M.toast({html: elm.innerHTML,
+            displayLength: 4000})
+    }
+)
+
+document.querySelectorAll(".flash-error").forEach(
+    (elm) => {
+        M.toast({html: elm.innerHTML, 
+            classes: "red-text pulse",
+            displayLength: 6000})
     }
 )
